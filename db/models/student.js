4ae02082f79c89f53a68eb6cfc8e4fb7e9200428
Michelle <michelle_broomes@yahoo.com>
2020-03-14 11:48:11 -0400

    passing Students model

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,30 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+});
+
+Student.beforeSave(student => {
+    let first = student.firstName;
+    let last = student.lastName;
+
+    student.firstName= first.charAt(0).toUpperCase() + first.slice(1);
+    student.lastName= last.charAt(0).toUpperCase() + last.slice(1);
+})
 
 module.exports = Student;
